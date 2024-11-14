158647f9a00353e4559f0b29e7dcd7392edd9177
unknown <mayer1990@gmail.com>
2024-11-14 02:03:01 -0300

    adding initial commit for spring security

---
+++ src/main/java/com/mayer/security/LoginController.java
@@ -0,0 +1,28 @@
+package com.mayer.security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "admin";
+    }
+}
